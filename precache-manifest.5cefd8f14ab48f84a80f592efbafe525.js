a90b3d00efc4102e04a52c6b912c9c5747a69a9e
kurockett <cxmdeluxe@yandex.by>
2021-02-22 11:34:25 +0300

    Updates

---
+++ precache-manifest.5cefd8f14ab48f84a80f592efbafe525.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a80ed33ef9ab64e540c8",
+    "url": "/portfolio/jobs/static/js/main.a80ed33e.chunk.js"
+  },
+  {
+    "revision": "b803bd81dd8dca302d9e",
+    "url": "/portfolio/jobs/static/js/1.b803bd81.chunk.js"
+  },
+  {
+    "revision": "5d62367e00de2a7ebca9",
+    "url": "/portfolio/jobs/static/css/2.a399ff5a.chunk.css"
+  },
+  {
+    "revision": "5d62367e00de2a7ebca9",
+    "url": "/portfolio/jobs/static/js/2.5d62367e.chunk.js"
+  },
+  {
+    "revision": "b92e99ff99a3e193db0d",
+    "url": "/portfolio/jobs/static/js/runtime~main.b92e99ff.js"
+  },
+  {
+    "revision": "6461caab5826556f0f3df06f7d6ebd50",
+    "url": "/portfolio/jobs/index.html"
+  }
+];
